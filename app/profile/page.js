765b37ebcc2606a43e118b92525b172b33c68c9c
Aman-Ingale <ingaleaman905@gmail.com>
2026-01-23 11:15:39 +0530

    initialize supabase

--- app/profile/page.js
+++ app/profile/page.js
@@ -1,7 +1,19 @@
-function profile() {
+"use client"
+import { useEffect, useState } from 'react'
+import { getProfile } from '../actions/profile.actions'
+
+export default async function Page() {
+    const [data,setData] = useState("")
+    
+    useEffect(()=>{
+      async function getData() {
+      setData(await getProfile())
+      }
+      getData()
+      console.log(data)
+    },[])
+    
   return (
-    <div>profile</div>
+    <></>
   )
 }
-
-export default page
